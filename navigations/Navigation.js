05a0fcd07104d888d1be99db208555f0133b1f95
Luis Santos <luissantoszun24@hotmail.com>
2021-02-22 17:24:33 -0500

    Add Tab icons

--- navigations/Navigation.js
+++ navigations/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Icon } from 'react-native-elements'
 
 import FavoritesStack from './FavoritesStack'
 import TopRestaurantsStack from './TopRestaurantsStack'
@@ -11,9 +12,48 @@ import RestaurantsStack from './RestaurantsStack'
 const Tab = createBottomTabNavigator()
 
 export default function Navigation() {
+    const screenOptions = (route, color) => {
+        let iconName
+        switch (route.name) {
+            case "restaurants":
+                iconName = "compass-outline"
+                break;
+            case "favorites":
+                iconName = "heart-outline"
+                break;
+            case "top-restaurants":
+                iconName = "start-outline"
+                break;
+            case "search":
+                iconName = "magnify"
+                break;
+            case "account":
+                iconName = "home-outline"
+                break;
+        }
+
+        return (
+            <Icon
+                type="material-community"
+                name={iconName}
+                size={22}
+                color={color}
+            />
+        )
+    }
+
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+                initialRouteName="restaurants"
+                tabBarOptions={{
+                    inactiveTintColor:"#a17dc3",
+                    activeTintColor:"#442484"
+                }}
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ color }) => screenOptions(route, color)
+                })}
+            >
                 <Tab.Screen
                     name="restaurants"
                     component={RestaurantsStack}

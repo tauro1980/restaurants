6a222d7734d1f3f633080ca6f6485f0ad3952b43
Luis Santos <luissantoszun24@hotmail.com>
2021-03-11 07:21:15 -0500

    Mostrar u ocultar la contraseña

--- components/account/RegisterForm.js
+++ components/account/RegisterForm.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Button, Input } from 'react-native-elements'
+import { Button, Icon, Input } from 'react-native-elements'
 
 export default function RegisterForm() {
+    const [showPassword, setShowPassword] = useState(false)
     return (
         <View style={styles.form}>
             <Input
@@ -13,13 +14,29 @@ export default function RegisterForm() {
                 containerStyle={styles.input}
                 placeholder="Ingresa tu contraseña..."
                 password={true}
-                secureTextEntry={true}
+                secureTextEntry={!showPassword}
+                rightIcon={
+                    <Icon
+                        type="material-community"
+                        name={showPassword ? "eye-off-outline" : "eye-outline"}
+                        iconStyle={styles.icon}
+                        onPress={() => setShowPassword(!showPassword)}
+                    />
+                }
             />
             <Input
                 containerStyle={styles.input}
                 placeholder="Confirma tu contraseña..."
                 password={true}
-                secureTextEntry={true}
+                secureTextEntry={!showPassword}
+                rightIcon={
+                    <Icon
+                        type="material-community"
+                        name={showPassword ? "eye-off-outline" : "eye-outline"}
+                        iconStyle={styles.icon}
+                        onPress={() => setShowPassword(!showPassword)}
+                    />
+                }
             />
             <Button
                 containerStyle={styles.btnContainer}
@@ -44,5 +61,8 @@ const styles = StyleSheet.create({
     },
     btn: {
         backgroundColor: "#442484"
+    },
+    icon: {
+        color: "#c1c1c1"
     }
 })

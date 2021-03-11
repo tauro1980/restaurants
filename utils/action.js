f870b85b87709d81399763a43d601ac0f057b4bb
Luis Santos <luissantoszun24@hotmail.com>
2021-03-11 10:58:48 -0500

    Implementar médotos para registrar usuario/cerrar sesión

--- utils/action.js
+++ utils/action.js
@@ -15,3 +15,18 @@ export const isUserLogged = () => {
 export const getCurrentUser = () => {
     return firebase.auth().currentUser
 }
+
+export const closeSession = () => {
+    return firebase.auth().signOut()
+}
+
+export const registerUser = async(email, password) => {
+    const result = { statusResponse: true, error: null }
+    try {
+        await firebase.auth().createUserWithEmailAndPassword(email, password)
+    } catch (error) {
+        result.statusResponse = false
+        result.error = "Este correo ya ha sido registrado"
+    }
+    return result
+}

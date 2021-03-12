25e6af3c5866c040da28fe2e6f16acba34069d6d
Luis Santos <luissantoszun24@hotmail.com>
2021-03-11 20:49:53 -0500

    Continuando con la pantalla de usuario

--- components/account/InfoUser.js
+++ components/account/InfoUser.js
@@ -1,12 +1,42 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Avatar } from 'react-native-elements'
 
 export default function InfoUser({ user }) {
     return (
-        <View>
-            <Text>InfoUser ...</Text>
+        <View style={styles.container}>
+            <Avatar
+                rounded={true}
+                size="large"
+                source={
+                    user.photoURL 
+                    ? { uri: photoURL } 
+                    : require("../../assets/avatar-default.jpg")
+                }
+            />
+            <View style={styles.infoUser}>
+                <Text style={styles.displayName}>
+                    {user.displayName ? user.displayName : "Anónimo"}
+                </Text>
+                <Text>{user.email}</Text>
+            </View>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "row",
+        backgroundColor: "#f9f9f9",
+        paddingVertical: 30
+    },
+    infoUser: {
+        marginLeft: 20
+    },
+    displayName: {
+        fontWeight: "bold",
+        paddingBottom: 5
+    }
+})

d34aea8f639b275d638054b0b356b4888eb1027a
Luis Santos <luissantoszun24@hotmail.com>
2021-03-11 22:21:10 -0500

    Actualizando la foto de perfil

--- components/account/InfoUser.js
+++ components/account/InfoUser.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Alert } from 'react-native'
 import { Avatar } from 'react-native-elements'
 
+import { updateProfile, uploadImage } from '../../utils/action'
 import { loadImageFromGallery } from '../../utils/helpers'
 
-export default function InfoUser({ user }) {
-    const changePhoto = async() =>{
-        const result = await loadImageFromGallery([1,1])
-        console.log(result)
+export default function InfoUser({ user, setLoading, setLoadingText }) {
+    const [photoUrl, setPhotoUrl] = useState(user.photoURL)
+
+    const changePhoto = async() => {
+        const result = await loadImageFromGallery([1, 1])
+        if (!result.status) {
+            return
+        }
+        setLoadingText("Actualizando imagen...")
+        setLoading(true)
+        const resultUploadImage = await uploadImage(result.image, "avatars", user.uid)
+        if (!resultUploadImage.statusResponse) {
+            setLoading(false)
+            Alert.alert("Ha ocurrido un error al almacenar la foto de perfil.")
+            return
+        }
+        const resultUpdateProfie = await updateProfile({ photoURL: resultUploadImage.url })
+        setLoading(false)
+        if (resultUpdateProfie.statusResponse) {
+            setPhotoUrl(resultUploadImage.url)
+        } else {
+            Alert.alert("Ha ocurrido un error al actualizar la foto de perfil.")
+        }
     }
 
     return (
@@ -15,11 +35,11 @@ export default function InfoUser({ user }) {
             <Avatar
                 rounded={true}
                 size="large"
-                onPress={changePhoto()}
+                onPress={changePhoto}
                 source={
-                    user.photoURL 
-                    ? { uri: photoURL } 
-                    : require("../../assets/avatar-default.jpg")
+                    photoUrl 
+                        ? { uri: photoUrl }
+                        : require("../../assets/avatar-default.jpg")
                 }
             />
             <View style={styles.infoUser}>

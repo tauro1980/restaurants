3260cc796ce7e92078490ae6f4b0bd440e918cce
Luis Santos <luissantoszun24@hotmail.com>
2021-03-13 07:39:22 -0500

    Botón flotante para agregar restaurantes

---
+++ screens/restaurants/Restaurants.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Icon } from 'react-native-elements'
+import firebase from 'firebase/app'
+
+import Loading from '../../components/Loading'
+
+export default function Restaurants() {
+    const [user, setUser] = useState(null)
+    
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((userInfo)=>{
+            userInfo ? setUser(true) : setUser(false)
+        })
+    }, [])
+
+    if(user===null){
+        return <Loading isVisible={true} text="Cargando..."/>
+    }
+
+    return (
+        <View style={styles.viewBody}>
+            <Text>Restaurants...</Text>
+            {            
+                user && (
+                    <Icon
+                        type="material-community"
+                        name="plus"
+                        color="#442484"
+                        reverse={true}
+                        containerStyle={styles.btnContainer}
+                    />
+                )
+            }        
+            </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    viewBody: {
+        flex: 1
+    },
+    btnContainer:{
+        position:"absolute",
+        bottom:10,
+        right:10,
+        shadowColor:"black",
+        shadowOffset: { width: 2, height:2 },
+        shadowOpacity: 0.5
+    }
+})

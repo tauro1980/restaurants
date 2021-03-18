2a1deabdf067641065f8f38184c47533e1994fec
Luis Santos <luissantoszun24@hotmail.com>
2021-03-18 11:19:15 -0500

    Mostrando lista de restaurantes

--- screens/restaurants/Restaurants.js
+++ screens/restaurants/Restaurants.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect, useCallback } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Icon } from 'react-native-elements'
 import { useFocusEffect } from '@react-navigation/native'
+import { size } from 'lodash'
 import firebase from 'firebase/app'
 
 import Loading from '../../components/Loading'
+import ListRestaurants from '../../components/restaurants/ListRestaurants'
 import { getRestaurants } from '../../utils/action'
 
 export default function Restaurants({ navigation }) {
@@ -14,7 +16,6 @@ export default function Restaurants({ navigation }) {
     const [loading, setLoading] = useState(false)
     
     const limitRestaurants = 7
-    console.log("restaurants", restaurants)
     
     useEffect(() => {
         firebase.auth().onAuthStateChanged((userInfo)=>{
@@ -40,7 +41,18 @@ export default function Restaurants({ navigation }) {
 
     return (
         <View style={styles.viewBody}>
-            <Text>Restaurants...</Text>
+            {
+                size(restaurants) > 0 ? (
+                    <ListRestaurants
+                        restaurants={restaurants}
+                        navigation={navigation}
+                    />
+                ) : (
+                    <View style={styles.notFoundView}>
+                        <Text style={styles.notFoundText}>No hay restaurantes registrados...</Text>
+                    </View>
+                )
+            }
             {            
                 user && (
                     <Icon
@@ -69,5 +81,14 @@ const styles = StyleSheet.create({
         shadowColor:"black",
         shadowOffset: { width: 2, height:2 },
         shadowOpacity: 0.5
+    },
+    notFoundView: {
+        flex: 1,
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    notFoundText:{
+        fontSize: 18,
+        fontWeight: "bold"
     }
 })

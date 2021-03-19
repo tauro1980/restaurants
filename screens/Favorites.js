1c626fb973385d9b035a58e45e48dbcc289def02
Luis Santos <luissantoszun24@hotmail.com>
2021-03-19 06:53:30 -0500

    Obtener la lista de restaurantes favoritos por usuario

--- screens/Favorites.js
+++ screens/Favorites.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React,{ useState, useCallback } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { useFocusEffect } from '@react-navigation/native'
+
+import { getFavorites } from '../utils/action'
 
 export default function Favorites() {
+    
+    useFocusEffect(
+        useCallback(() => {
+            async function getData() {
+                const response = await getFavorites()
+                console.log(response)
+            }
+            getData()
+        }, [])
+    )
+    
     return (
         <View>
             <Text>Favorites</Text>

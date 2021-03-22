b1aad16cd97c56e65b2aa27389f5f387fc53aec8
Luis Santos <luissantoszun24@hotmail.com>
2021-03-22 11:59:13 -0500

    Creando método para buscar restaurantes

--- screens/Search.js
+++ screens/Search.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { isEmpty } from 'lodash'
 
-export default function Search() {
+import { seacrhRestaurants } from '../utils/action'
+
+export default function Search({ navigation }) {
+    const [search, setSearch] = useState("E")
+    const [restaurants, setRestaurants] = useState([])
+
+    console.log(restaurants)
+
+    useEffect(() => {
+        if(isEmpty(search)){
+            return
+        }
+
+        async function getData() {
+            const response = await seacrhRestaurants(search)
+            if(response.statusResponse){
+                setRestaurants(response.restaurants)
+            }
+        }
+        getData()
+    }, [search])
+
     return (
         <View>
             <Text>Search</Text>

e7911cb38a658682e2542b9818ed857475da052a
Luis Santos <luissantoszun24@hotmail.com>
2021-03-22 22:05:08 -0500

    Ad. métodos para llamar, enviar correo y enviar WhatsApp

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,7 @@
 import * as Permisions from 'expo-permissions'
 import * as ImagePicker from 'expo-image-picker'
 import * as Location from 'expo-location';
-import { Alert } from 'react-native'
+import { Alert, Linking } from 'react-native'
 
 export function validateEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -54,4 +54,23 @@ export const getCurrentLocation = async() => {
 
 export const formatPhone = (callingCode, phone) => {
     return `+(${callingCode}) ${phone.substr(0,2)} ${phone.substr(2,3)} ${phone.substr(5,4)}`
+}
+
+export const callNumber = (phoneNumber) => {
+    Linking.openURL(`tel:${phoneNumber}`)
+}
+
+export const sendWhatsApp = (phoneNumber, text) => {
+    const link = `htpps://wa.me/${phoneNumber}?text=${text}`
+    Linking.canOpenURL(link).then((supported) => {
+        if(!supported){
+            Alert.alert("Por favor instale WhatsApp para enviar un mensaje directo.")
+            return
+        }
+        return Linking.openURL(link)
+    })
+}
+
+export const sendEmail = (to, subject, body) => {
+    Linking.openURL(`mailto:${to}?subject:${subject}&body${body}`)
 }
